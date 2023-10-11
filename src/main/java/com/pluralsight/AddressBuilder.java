33c888def730fc26fc6657f241c955cabb99b7ec
Sing Tuttanon <singtuttanon@gmail.com>
2023-10-11 10:18:33 -0400

    Address Builder Completed

--- src/main/java/com/pluralsight/AddressBuilder.java
+++ src/main/java/com/pluralsight/AddressBuilder.java
@@ -6,52 +6,48 @@ public class AddressBuilder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder addressInfo = new StringBuilder();
-
-        System.out.println("Please enter the following information below:");
-
-        // Prompt the user for billing information
+//  User inputs
+        System.out.println("Please enter the following information below: ");
         System.out.print("Full Name: ");
         String fullName = scanner.nextLine();
-        addressInfo.append("Full Name: ").append(fullName).append("\n");
+        addressInfo.append(fullName).append("\nBilling Address:\n");
 
         System.out.print("Billing Street: ");
         String billingStreet = scanner.nextLine();
-        addressInfo.append("Billing Street: ").append(billingStreet).append("\n");
+        addressInfo.append(billingStreet).append("\n");
 
         System.out.print("Billing City: ");
         String billingCity = scanner.nextLine();
-        addressInfo.append("Billing City: ").append(billingCity).append("\n");
+        addressInfo.append(billingCity).append(", ");
 
         System.out.print("Billing State (Abbreviated): ");
         String billingState = scanner.nextLine();
-        addressInfo.append("Billing State: ").append(billingState).append("\n");
+        addressInfo.append(billingState).append(" ");
 
         System.out.print("Billing Zip code: ");
         String billingZip = scanner.nextLine();
-        addressInfo.append("Billing Zip code: ").append(billingZip).append("\n");
+        addressInfo.append(billingZip).append("\nShipping Address:\n");
 
-        // Prompt the user for shipping information
         System.out.print("Shipping Street: ");
         String shippingStreet = scanner.nextLine();
-        addressInfo.append("\nShipping Street: ").append(shippingStreet).append("\n");
+        addressInfo.append(shippingStreet).append("\n");
 
         System.out.print("Shipping City: ");
         String shippingCity = scanner.nextLine();
-        addressInfo.append("Shipping City: ").append(shippingCity).append("\n");
+        addressInfo.append(shippingCity).append(", ");
 
         System.out.print("Shipping State: ");
         String shippingState = scanner.nextLine();
-        addressInfo.append("Shipping State: ").append(shippingState).append("\n");
+        addressInfo.append(shippingState).append(" ");
 
         System.out.print("Shipping Zip code: ");
         String shippingZip = scanner.nextLine();
-        addressInfo.append("Shipping Zip code: ").append(shippingZip);
-
-        // Display customer information
+        addressInfo.append(shippingZip);
+//  Display Information in a specific format
         System.out.println("\nCustomer Information:");
         System.out.println(addressInfo.toString());
 
-        // Close the scanner
+//  Close the scanner
         scanner.close();
     }
 }
